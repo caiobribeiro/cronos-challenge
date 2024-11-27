5fbde5ccd3447c071a29b2a053920d6ec749bcd9
Caio Beraldi Ribeiro <caio_ribeiro@protonmail.com>
2024-11-27 06:30:08 -0300

    feat: add new AstronomyPictureEntity

---
+++ lib/features/apod_nasa/domain/entities/astronomy_picture_entity.dart
@@ -0,0 +1,20 @@
+class AstronomyPictureEntity {
+  String? copyright;
+  String? date;
+  String? explanation;
+  String? hdurl;
+  String? mediaType;
+  String? serviceVersion;
+  String? title;
+  String? url;
+  AstronomyPictureEntity({
+    this.copyright,
+    this.date,
+    this.explanation,
+    this.hdurl,
+    this.mediaType,
+    this.serviceVersion,
+    this.title,
+    this.url,
+  });
+}
